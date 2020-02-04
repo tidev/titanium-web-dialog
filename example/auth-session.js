3552ecb92000ee76091f29276d5c33a16d7f4e1f
Chris Williams <chris.a.williams@gmail.com>
2020-02-04 10:06:15 -0500

    style: fix eslint issues

--- example/auth-session.js
+++ example/auth-session.js
@@ -1,33 +1,33 @@
 var WebDialog = require('ti.webdialog');
 
 var authSession = WebDialog.createAuthenticationSession({
-  url: 'http://0.0.0.0:5000/get-cookie/user?callbackUrl=testwebdialog://', // Your OAuth-URL + app-scheme as callback-URL
-  scheme: 'testwebdialog://', // app-scheme
+	url: 'http://0.0.0.0:5000/get-cookie/user?callbackUrl=testwebdialog://', // Your OAuth-URL + app-scheme as callback-URL
+	scheme: 'testwebdialog://', // app-scheme
 });
 
 // Invoked when the server returns a callback-URL
 authSession.addEventListener('callback', function (event) {
-  if (!event.success) {
-    Ti.API.error('Error authenticating: ' + event.error);
-    return;
-  }
+	if (!event.success) {
+		Ti.API.error('Error authenticating: ' + event.error);
+		return;
+	}
 
-  Ti.API.info('Callback URL: ' + event.callbackURL);
+	Ti.API.info('Callback URL: ' + event.callbackURL);
 });
 
 var win = Ti.UI.createWindow({
-  backgroundColor: '#fff'
+	backgroundColor: '#fff'
 });
 
 var btn = Ti.UI.createButton({
-  title: 'Start OAuth-session'
+	title: 'Start OAuth-session'
 });
 
 btn.addEventListener('click', function () {
-  if (!authSession.isSupported()) {
-    return Ti.API.error('This API is iOS 11+ only');
-  }
-  authSession.start(); // Or cancel() to cancel it manually.
+	if (!authSession.isSupported()) {
+		return Ti.API.error('This API is iOS 11+ only');
+	}
+	authSession.start(); // Or cancel() to cancel it manually.
 });
 
 win.add(btn);
